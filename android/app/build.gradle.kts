659b9552de9dc6651c2075929b0fd3664bcfadfa
Niral-09 <niral0901@gmail.com>
2026-04-05 22:52:43 +0530

    fix: use Kotlin 2.0 Compose plugin, add READ_EXTERNAL_STORAGE fallback for API 26-32

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    id("org.jetbrains.kotlin.plugin.compose")
     id("com.google.dagger.hilt.android")
     id("com.google.devtools.ksp")
 }
@@ -33,8 +34,6 @@ android {
     kotlinOptions { jvmTarget = "17" }
 
     buildFeatures { compose = true }
-
-    composeOptions { kotlinCompilerExtensionVersion = "1.5.14" }
 
     packaging {
         resources {
